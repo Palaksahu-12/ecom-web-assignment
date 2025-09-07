b622af3149e49da6a52693b4119c6b4d7ccd45fb
Palak Sahu <palaksahu2233@gmail.com>
2025-09-07 21:20:05 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -98,4 +98,4 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get(/^\/.*$/, (req, res) => { res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`✅ Backend running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
