ea49f25bb2802cd38879cd6e269fe12661cdd8f1
Palak Sahu <palaksahu2233@gmail.com>
2025-09-07 16:01:16 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -90,9 +90,10 @@ app.post("/api/cart/remove", auth, (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/:path(*)", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
